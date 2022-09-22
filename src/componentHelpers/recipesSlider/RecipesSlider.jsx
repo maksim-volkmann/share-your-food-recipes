c1a392bf943defccdb821dc05f295fb16fc5f8d0
Inese Lapina <lapinna.inese@gmail.com>
2022-09-22 16:16:10 +0200

    refactor recipes component

---
+++ src/componentHelpers/recipesSlider/RecipesSlider.jsx
@@ -0,0 +1,37 @@
+import "./recipesSlider.scss";
+import useRecipesHook from "../../customHooks/useRecipesHook";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import { Link } from "react-router-dom";
+
+const RecipesSlider = () => {
+  const recipesState = useRecipesHook();
+  return (
+    <div className="recipesWrapper">
+      <Splide
+        options={{
+          perPage: 4,
+          arrows: false,
+          pagination: false,
+          drag: "free",
+          gap: "2rem",
+        }}
+      >
+        {recipesState.map((recipe) => {
+          return (
+            <SplideSlide>
+              <div className="recipe" key={recipe.id}>
+                <Link to={"/recipe/" + recipe.id}>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Link>
+              </div>
+            </SplideSlide>
+          );
+        })}
+      </Splide>
+    </div>
+  );
+};
+
+export default RecipesSlider;
--- src/components/recipes/Recipes.jsx
+++ src/components/recipes/Recipes.jsx
@@ -1,42 +1,17 @@
 import "./recipes.scss";
-import useRecipesHook from "../../customHooks/useRecipesHook";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css";
-import { Link } from "react-router-dom";
+
 import Newsletter from "../../componentHelpers/newsletter/Newsletter";
+import RecipesSlider from "../../componentHelpers/recipesSlider/RecipesSlider";
 
 function Recipes() {
-  const recipesState = useRecipesHook();
-
   return (
-    <div className="recipesWrapper">
-      <Splide
-        options={{
-          perPage: 4,
-          arrows: false,
-          pagination: false,
-          drag: "free",
-          gap: "2rem",
-        }}
-      >
-        {recipesState.map((recipe) => {
-          return (
-            <SplideSlide>
-              <div className="recipe" key={recipe.id}>
-                <Link to={"/recipe/" + recipe.id}>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                </Link>
-              </div>
-            </SplideSlide>
-          );
-        })}
-      </Splide>
+    <>
+      <RecipesSlider />
       <Newsletter
         title={"Join our newletter and get the latest recipes!"}
         btnTitle={"Send"}
       />
-    </div>
+    </>
   );
 }
 
