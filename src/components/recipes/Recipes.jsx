5f79384538c2a154af5fc7a09f4c6be9a5fd237f
Inese Lapina <lapinna.inese@gmail.com>
2022-09-23 12:06:03 +0200

    add initial displayNewRecipe component

--- src/components/recipes/Recipes.jsx
+++ src/components/recipes/Recipes.jsx
@@ -2,10 +2,24 @@ import "./recipes.scss";
 import Newsletter from "../../componentHelpers/newsletter/Newsletter";
 import RecipesSlider from "../../componentHelpers/recipesSlider/RecipesSlider";
 import Categories from "../../componentHelpers/categories/Categories";
+import DisplayNewRecipe from "../../componentHelpers/displayNewRecipe/DisplayNewRecipe";
 
 function Recipes() {
   return (
     <>
+      <DisplayNewRecipe
+        titleLeft="NEW1"
+        dateLeft="Date"
+        authorLeft="Author"
+        descriptionLeft="Short Recipe Summary"
+        imageLeft="https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg"
+        titleRight="NEW2"
+        dateRight="Date"
+        authorRight="Author"
+        descriptionRight="Short Recipe Summary"
+        imageRight="https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg"
+        
+      />
       <RecipesSlider title={"Popular Picks"} />
       <Categories
         title={"All Categories"}
