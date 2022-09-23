6d6a1f112d9ad835bacb24e80745966798bc040e
Maksim <auksinistonic@gmail.com>
2022-09-23 09:46:02 +0300

    Revert "Add new functionalities from dev"

--- src/components/homeBody/HomeBody.jsx
+++ src/components/homeBody/HomeBody.jsx
@@ -1,26 +1,11 @@
-import "./homeBody.scss";
-import HomeBodySlider from "../../componentHelpers/homeBodySlider/HomeBodySlider";
-import Newsletter from "../../componentHelpers/newsletter/Newsletter";
+import './homeBody.scss'
 
 const HomeBody = () => {
   return (
     <div className="homeBody">
-      <div className="homeBodyWrapper">
-        <HomeBodySlider
-          sliderTitle="Featured Recipes"
-          author="Author"
-          date="Date"
-          category="Category"
-          summary="Summary Of The Recipe"
-          btnTitle="Make this"
-        />
-        <Newsletter
-          title={"Join our newletter and get the latest recipes!"}
-          btnTitle={"Send"}
-        />
-      </div>
+      <div className="homeBodyWrapper">HOMEBODY</div>
     </div>
-  );
-};
+  )
+}
 
-export default HomeBody;
+export default HomeBody
