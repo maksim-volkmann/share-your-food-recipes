830aa36f9691f3f0bd9996f8a9c67b09ab0b37ff
Inese Lapina <lapinna.inese@gmail.com>
2022-09-26 00:13:29 +0200

    fix sliders

--- src/componentHelpers/homeBodySlider/HomeBodySlider.jsx
+++ src/componentHelpers/homeBodySlider/HomeBodySlider.jsx
@@ -3,41 +3,42 @@ import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import useRecipesHook from "../../customHooks/useRecipesHook";
 import { useNavigate } from "react-router-dom";
+import { useState, useMemo } from "react";
 
 const HomeBodySlider = ({
   sliderTitle,
-  author,
-  date,
-  category,
   summary,
   btnTitle,
 }) => {
   const recipesState = useRecipesHook();
   const navigate = useNavigate();
+  const [maxRecipes, setMaxRecipes] = useState(12);
+  const  reducedRecipes = useMemo(() => {
+    return recipesState.slice(0, maxRecipes);
+  }, [maxRecipes, recipesState]);
+
   return (
     <div className="sliderContainer">
       <h1>{sliderTitle}</h1>
       <div className="sliderWrapper">
         <Splide options={{ rewind: true, type: "fade" }}>
-          {recipesState.map((recipe) => {
+          {reducedRecipes.map((recipe) => {
             return (
               <SplideSlide>
                 <div className="sliderItem" key={recipe.id}>
                   <div className="imgWrapper">
-                    <img src={recipe.image} alt={recipe.title} />
+                    <img src={recipe.mainImage} alt={recipe.name} />
                   </div>
                   <div className="slideContent">
-                    <h2>{recipe.title}</h2>
+                    <h2>{recipe.name}</h2>
                     <p>
                       {"by "}
-                      <span className="author">{author}</span>
+                      <span className="author">{recipe.author}</span>
                       {" | "}
-                      <span className="date">{date}</span>
-                      {" | "}
-                      <span className="category">{category}</span>
+                      <span className="category">{recipe.goodFor}</span>
                     </p>
                     <h3>{summary}</h3>
-                    <button onClick={() => navigate("/recipe/" + recipe.id)}>
+                    <button onClick={() => navigate("/recipe/" + recipe._id)}>
                       {btnTitle}
                     </button>
                   </div>
