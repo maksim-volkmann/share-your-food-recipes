1c5e3d4a45cc3178b6cb647cc15c58f1d4e05878
Maksim <auksinistonic@gmail.com>
2022-09-26 12:30:36 +0300

    delete: remove test backend from frontend

--- backend/routes/recipeRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createRecipe } from "../controller/recipeController.js";
-
-const router = express.Router();
-
-router.post("/addRecipe", createRecipe);
-
-export default router;
