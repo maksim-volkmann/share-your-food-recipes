2545f20393df7895af61599740aa3f1c86a2440f
Maksim <auksinistonic@gmail.com>
2022-09-26 05:22:51 +0300

    update: add recipe page styles

--- src/componentHelpers/homeBodySlider/HomeBodySlider.jsx
+++ src/componentHelpers/homeBodySlider/HomeBodySlider.jsx
@@ -1,27 +1,23 @@
-import "./homeBodySlider.scss";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css";
-import useRecipesHook from "../../customHooks/useRecipesHook";
-import { useNavigate } from "react-router-dom";
-import { useState, useMemo } from "react";
+import './homeBodySlider.scss'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css'
+import useRecipesHook from '../../customHooks/useRecipesHook'
+import { useNavigate } from 'react-router-dom'
+import { useState, useMemo } from 'react'
 
-const HomeBodySlider = ({
-  sliderTitle,
-  summary,
-  btnTitle,
-}) => {
-  const recipesState = useRecipesHook();
-  const navigate = useNavigate();
-  const [maxRecipes, setMaxRecipes] = useState(12);
-  const  reducedRecipes = useMemo(() => {
-    return recipesState.slice(0, maxRecipes);
-  }, [maxRecipes, recipesState]);
+const HomeBodySlider = ({ sliderTitle, summary, btnTitle }) => {
+  const recipesState = useRecipesHook()
+  const navigate = useNavigate()
+  const [maxRecipes, setMaxRecipes] = useState(6)
+  const reducedRecipes = useMemo(() => {
+    return recipesState.slice(0, maxRecipes)
+  }, [maxRecipes, recipesState])
 
   return (
     <div className="sliderContainer">
       <h1>{sliderTitle}</h1>
       <div className="sliderWrapper">
-        <Splide options={{ rewind: true, type: "fade" }}>
+        <Splide options={{ rewind: true, type: 'fade' }}>
           {reducedRecipes.map((recipe) => {
             return (
               <SplideSlide>
@@ -32,24 +28,24 @@ const HomeBodySlider = ({
                   <div className="slideContent">
                     <h2>{recipe.name}</h2>
                     <p>
-                      {"by "}
+                      {'by '}
                       <span className="author">{recipe.author}</span>
-                      {" | "}
+                      {' | '}
                       <span className="category">{recipe.goodFor}</span>
                     </p>
                     <h3>{summary}</h3>
-                    <button onClick={() => navigate("/recipe/" + recipe._id)}>
+                    <button onClick={() => navigate('/recipe/' + recipe._id)}>
                       {btnTitle}
                     </button>
                   </div>
                 </div>
               </SplideSlide>
-            );
+            )
           })}
         </Splide>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default HomeBodySlider;
+export default HomeBodySlider
--- src/components/recipe/Recipe.jsx
+++ src/components/recipe/Recipe.jsx
@@ -11,19 +11,19 @@ function Recipe() {
   console.log('SINGLE RECIPE')
   // console.log(id)
   console.log(recipeState)
-  const navigate = useNavigate();
-  
+  const navigate = useNavigate()
+
   const test1 = recipeState
     .filter((recipeID) => recipeID._id === id)
     .map((recipe) => {
       return (
         <>
-          <div className="recipeWrapper" >
+          <div className="recipeWrapper">
             <h1>{recipe.name}</h1>
-            <button onClick={() => navigate("/update/" + recipe._id)}>UPDATE</button>
-            <DeleteRecipeButton
-            btnTitle="Delete"
-            />
+            <button onClick={() => navigate('/update/' + recipe._id)}>
+              UPDATE
+            </button>
+            <DeleteRecipeButton btnTitle="Delete" />
             <div className="textWrapper">
               <div className="textWrap">
                 <h4>Servings:</h4>
@@ -42,30 +42,38 @@ function Recipe() {
                 <p>{recipe.goodFor}</p>
               </div>
             </div>
+
             <div className="introduction">
-              <div className="introductionWrapper">
+              <div className="introductionWrapper intro-text">
                 <h2>{recipe.name}</h2>
                 <h4>By: {recipe.author}</h4>
                 <p>{recipe.description1}</p>
               </div>
-              <div className="introductionWrapper">
+              <div className="introductionWrapper intro-image">
                 <img src={recipe.mainImage} alt={recipe.name} />
               </div>
             </div>
+
             <div className="recipeInfo">
-                <div className="recipeIngredients">
-                  <h2>Ingredients</h2>
-                  <p>
-                    {recipe.ingredients}
-                  </p>
-                </div>
-                <div className="recipeInstructions">
-                  <h2>Instructions</h2>
-                  <p>
-                    {recipe.description2}
-                  </p>
-                </div>
+              <div className="recipeIngredients">
+                <h2>Ingredients</h2>
+                <ul className="ingrList">
+                  {recipe.ingredients.split(',').map((ingr) => {
+                    return <li>{ingr}</li>
+                  })}
+                </ul>
               </div>
+              <div className="recipeInstructions">
+                <p>{recipe.description2}</p>
+              </div>
+            </div>
+
+            <div className="images-box">
+              <div className="image-box-wrapper">
+                <img src={recipe.image2} alt={recipe.name} />
+                <img src={recipe.image3} alt={recipe.name} />
+              </div>
+            </div>
           </div>
         </>
       )
